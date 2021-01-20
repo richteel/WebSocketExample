17dfe46c16f1455f06c9f8766653bb70a00c2fdf
Richard Teel <richteel@teelsys.com>
2021-01-19 21:41:18 -0500

    Documentation Update

--- Backend/src/Chat.php
+++ Backend/src/Chat.php
@@ -6,45 +6,43 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
+	public function __construct() {
+			$this->clients = new \SplObjectStorage;
+	}
 
-    public function __construct()
-    {
-        $this->clients = new \SplObjectStorage;
-    }
+	public function onOpen(ConnectionInterface $conn) {
+		// Store the new connection in $this->clients
+		$this->clients->attach($conn);
 
-    public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection in $this->clients
-        $this->clients->attach($conn);
+		echo "New connection! ({$conn->resourceId})\n";
+	}
 
-        echo "New connection! ({$conn->resourceId})\n";
-    }
+	public function onMessage(ConnectionInterface $from, $msg) {
+		$numRecv = count($this->clients) - 1;
+		echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+			, $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
-    public function onMessage(ConnectionInterface $from, $msg) {
-	$numRecv = count($this->clients) - 1;
-	echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+		foreach ( $this->clients as $client ) {
+			if ( $from->resourceId == $client->resourceId ) {
+				continue;
+			}
 
-        foreach ( $this->clients as $client ) {
-	    echo "There\n";
+			$client->send( "Client $from->resourceId said $msg" );
+		}
+	}
 
-            if ( $from->resourceId == $client->resourceId ) {
-                continue;
-            }
+	public function onClose(ConnectionInterface $conn) {
+		// The connection is closed, remove it, as we can no longer send it messages
+		$this->clients->detach($conn);
 
-            $client->send( "Client $from->resourceId said $msg" );
-        }
-    }
+		echo "Connection {$conn->resourceId} has disconnected\n";
+	}
 
-    public function onClose(ConnectionInterface $conn) {
-	// The connection is closed, remove it, as we can no longer send it messages
-        $this->clients->detach($conn);
+	public function onError(ConnectionInterface $conn, \Exception $e) {
+		echo "An error has occurred: {$e->getMessage()}\n";
 
-	echo "Connection {$conn->resourceId} has disconnected\n";
-    }
-
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-	echo "An error has occurred: {$e->getMessage()}\n";
-
-        $conn->close();
-    }
+		$conn->close();
+	}
 }
+
+?>
